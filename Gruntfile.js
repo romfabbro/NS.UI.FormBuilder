14c91c00aa3164eb4e54d9b67d56019d39df01bf
amiceli <miceli.antoine@gmail.com>
2014-10-17 12:02:05 +0200

    Add css map file

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,9 @@ module.exports = function(grunt) {
                 options: {
                     paths: ["stylesheet"],
                     cleancss: true,
+                    sourceMap: true,
+                    sourceMapFilename: 'compressed/formbuilder.css.map',
+                    sourceMapRootpath: ''
                 },
                 files: {
                     "compressed/formbuilder.min.css": "assets/stylesheet/styles.less"
@@ -20,6 +23,13 @@ module.exports = function(grunt) {
             stylesheet: {
                 files: ['assets/stylesheet/*.less'],
                 tasks: ['less:dist']
+                /*options: {
+                  livereload: true,
+                },*/
+            },
+            bower : {
+                files : ['bower.json'],
+                tasks : ['bower:target']
             }
         },
 
@@ -34,7 +44,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-bower-requirejs');
-    grunt.loadNpmTasks('grunt-shell');
 
-    grunt.registerTask('install', ['bower']);
 }
