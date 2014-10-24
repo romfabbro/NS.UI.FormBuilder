115b8dad52ec6be46f06b5dbc21bf39cbc52a25c
amiceli <miceli.antoine@gmail.com>
2014-10-24 11:14:59 +0200

    Update build file and configuration

--- build/build.js
+++ build/build.js
@@ -4,121 +4,48 @@
         backbone              : "../../libs/backbone/backbone",
         blobjs                : "../../libs/blobjs/Blob",
         bootstrap             : "../../libs/bootstrap/dist/js/bootstrap",
-        fancytree             : "../../libs/fancytree/dist/jquery.fancytree.min",
+        fancytree             : "../../libs/fancytree/dist/jquery.fancytree-all.min",
         filesaver             : "../../libs/filesaver/FileSaver",
         i18n                  : "../../libs/i18n/i18next",
         jquery                : "../../libs/jquery/dist/jquery",
-        jqueryui              : "../../libs/jquery-ui/jquery-ui.min",
+        'jquery-ui'           : "../../libs/jquery-ui/jquery-ui",
         underscore            : "../../libs/underscore/underscore",
         "NS.UI.Navbar"        : "../../libs/NS.UI.Navbar/navbar",
         "NS.UI.NavbarTheme"   : "../../libs/NS.UI.Navbar/themes/navbar.bootstrap3",
         "NS.UI.Notification"  : "../../libs/NS.UI.Notification/notification",
         requirejs             : "../../libs/requirejs/require",
-        "font-awesome"        : "../../libs/font-awesome/fonts/*",
-        "backbone-forms"      : "../../libs/backbone-forms/distribution.amd/backbone-forms",
-        "backbone-forms-list" : "../../libs/backbone-forms//distribution.amd/editors/list.min",
+        "backbone-forms"      : "../../libs/backbone-forms/distribution.amd/backbone-forms.min",
         modalAdapter          : "../../libs/bootstrapAdapter/src/backbone.bootstrap-modal",
+        "backbone-forms-list" : "../../libs/backbone-forms/distribution.amd/editors/list.min",
         bootstrapTemplate     : "../../libs/backbone-forms/distribution/templates/bootstrap3",
-        template              : "../../libs/backbone-forms/distribution/templates/bootstrap",
         bootstrapAdapter      : "../../libs/bootstrapAdapter/index",
         nanoscroller          : "../../libs/nanoscroller/dist/javascripts/jquery.nanoscroller",
         typeahead             : "../../libs/typeahead/bootstrap3-typeahead",
         "backbone.radio"      : "../../libs/backbone.radio/build/backbone.radio",
         fuelux                : "../../libs/fuelux/dist/js/fuelux",
-        text                  : '../../assets/js/text'
+        'difflib'             : '../../libs/jsdifflib/difflib',
+        'diffview'            : '../../libs/jsdifflib/diffview',
+        'twig'             : '../../libs/twig.js/twig.min'
     },
 
-    shim: {
-        jquery: {
-            exports: "$"
-        },
-        underscore: {
-            exports: "_"
-        },
-        backbone: {
-            deps: [
-                "underscore",
-                "jquery"
-            ],
-            exports: "Backbone"
-        },
-        "backbone-forms": {
-            deps: [
-                "backbone"
-            ],
-            exports: "Backbone"
-        },
-        "backbone.radio": {
-            deps: [
-                "backbone"
-            ],
-            exports: "Backbone"
-        },
-        bootstrapTemplate: {
-            deps: [
-                "backbone-forms"
-            ],
-            exports: "Form"
-        },
-        jqueryui: {
-            exports: "$",
-            deps: [
-                "jquery"
-            ]
-        },
-        fancytree: {
-            exports: "$",
-            deps: [
-                "jquery"
-            ]
-        },
-        i18n: {
-            exports: "$",
-            deps: [
-                "jquery"
-            ]
-        },
-        "NS.UI.Navbar": {
-            exports: "$",
-            deps: [
-                "jquery",
-                "backbone",
-                "bootstrap"
-            ]
-        },
-        "NS.UI.NavbarTheme": {
-            deps: [
-                "NS.UI.Navbar"
-            ],
-            exports: "NS"
-        },
-        "NS.UI.Notification": {
-            exports: "$",
-            deps: [
-                "jquery",
-                "backbone",
-                "bootstrap"
-            ]
-        },
-        bootstrap: {
-            exports: "$",
-            deps: [
-                "jquery"
-            ]
-        },
-        typeahead: {
-            exports: "$",
-            deps: [
-                "jquery",
-                "bootstrap"
-            ]
-        },
-        nanoscroller: {
-            exports: "$",
-            deps: [
-                "jquery"
-            ]
-        }
+   shim: {
+        blobjs                : { exports : 'blobjs' },
+        filesaver             : { exports : 'filesaver' },
+        difflib               : { exports : 'difflib' },
+        diffview              : { exports : 'diffview'},
+        jquery                : { exports: "$"},
+        underscore            : { exports: "_"},
+        backbone              : { exports: "Backbone", deps: ["underscore", "jquery"] },
+        "backbone.radio"      : { deps: ["backbone"], exports: "Backbone"},
+        "jquery-ui"           : { exports: "$", deps: ["jquery"] },
+        fancytree             : { deps: ["jquery-ui"], exports: "$" },
+        i18n                  : { exports: "$", deps: ["jquery"] },
+        "NS.UI.Navbar"        : { exports: "$", deps: ["jquery", "backbone", "bootstrap"] },
+        "NS.UI.NavbarTheme"   : { deps: ["NS.UI.Navbar"], exports: "NS"},
+        "NS.UI.Notification"  : { exports: "$", deps: ["jquery", "backbone", "bootstrap"] },
+        bootstrap             : { exports: "$", deps: ["jquery"] },
+        typeahead             : { exports: "$", deps: ["jquery", "bootstrap"] },
+        nanoscroller          : { exports: "$", deps: ["jquery"] }
     },
 
     optimize: 'uglify2',
@@ -140,6 +67,7 @@
         beautify: true
     },
     preserveLicenseComments : false,
+    removeCombined: true,
 
     include : [
         'views/fieldViews/TextFieldView',
@@ -156,11 +84,11 @@
         'views/fieldViews/RadioFieldView',
         'views/fieldViews/SelectFieldView',
         'views/fieldViews/SubFormFieldView',
-        'views/fieldViews/TableFieldView',
         'views/fieldViews/TreeViewFieldView',
         'views/modals/exportProtocol',
         'views/modals/importProtocol',
-        'views/modals/saveProtocol'
+        'views/modals/saveProtocol',
+        'views/fieldViews/ThesaurusFieldView'
     ]
 
 })
