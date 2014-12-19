28117b71c748630df56c8665a0bff649dae2741a
amiceli <miceli.antoine@gmail.com>
2014-12-19 18:10:06 +0100

    Update grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,13 @@ module.exports = function(grunt) {
             dist: {
                 options: {
                     paths: ["stylesheet"],
-                    cleancss: true,
-                    sourceMap: true,
+                    cleancss: false,
+                    sourceMap: false,
                     sourceMapFilename: 'compressed/formbuilder.css.map',
                     sourceMapRootpath: ''
                 },
                 files: {
-                    "compressed/formbuilder.min.css": "assets/stylesheet/styles.less"
+                    "compressed/formbuilder.min.css": "assets/stylesheet/all.less"
                 }
             }
         },
@@ -22,27 +22,21 @@ module.exports = function(grunt) {
         watch: {
             stylesheet: {
                 files: ['assets/stylesheet/*.less'],
-                tasks: ['less:dist']
-                /*options: {
-                  livereload: true,
-                },*/
-            },
-            bower : {
-                files : ['bower.json'],
-                tasks : ['bower:target']
+                tasks: ['less:dist', 'autoprefixer:single_file']
             }
         },
 
-        //  Bower : install bower components and create requireJS configuration file
-        bower: {
-            target: {
-                rjsConfig: 'assets/js/config.js'
-            }
+        autoprefixer: {
+            single_file: {
+                src: "compressed/formbuilder.min.css",
+                dest: "compressed/prefixedformbuilder.min.css"
+            },
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-requirejs');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
 }
